6bf1c2256a4442a73d85edd427563033631dd338
Adilson <adilsonchy@gmail.com>
2026-03-05 18:33:11 -0300

    feat: adicionar estrutura base

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
+      <Tasks />
+      <AddTask />
+
+      
+    </div>
+  );
+}
+
+export default App;
