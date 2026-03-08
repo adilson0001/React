1b62fa304c93bad0842b160b8e7379256003b15a
Adilson <adilsonchy@gmail.com>
2026-03-08 19:52:50 -0300

    feat: termina o cabeçalho

--- teste2/src/components/Header.tsx
+++ teste2/src/components/Header.tsx
@@ -1,13 +1,25 @@
-import "./Header.css"
-
 
 const Header = () => {
   return (
-    <div className="flex justify-between align-center max-w-300 m-auto w-full">
-      <img src="./Logo.png" alt=""  className="w-[250px] h-[60px]"/>
-      <p className="bg-[#00A2F7] w-[156px] h-[35px] flex justify-center items-center rounded-[8px] text-white font-bold cursor-pointer">Premium</p>
+    <div className="flex justify-between align-center max-w-300 my-6 w-full mx-auto">
+      <img
+        src="./Logo.png"
+        alt=""
+        className="w-62.5
+       h-15"
+      />
+      <p
+        className="bg-[#00A2F7] w-39 h-8.75 flex justify-center items-center rounded-lg text-white font-bold cursor-pointer 
+shadow-[4px_4px_8px_3px_rgba(0,0,0,0.20)]
+transform transition-all duration-200 ease-out
+hover:scale-105
+hover:shadow-[2px_2px_6px_2px_rgba(0,0,0,0.15)]"
+      >
+        Premium
+      </p>
     </div>
   );
 };
 
 export default Header;
+
--- teste2/src/main.tsx
+++ teste2/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header.tsx'
 import Footer from './components/Footer.tsx'
+import Pesq from './components/Pesq.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className='flex flex-col justify-between h-screen bg-[#F4FBFF]'>
+    <div className='flex flex-col h-screen  bg-[#F4FBFF]'>
       <Header></Header>
-      <div className='flex-1'>
-        <p >banana</p>
-      </div>
+      
+      <Pesq></Pesq>
       <Footer></Footer>
     </div>
   </StrictMode>,
